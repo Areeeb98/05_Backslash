67fdef7b9eadfdd72ebcf1ebd909ed2f58f2f8ab
Areeeb98 <122514572+Areeeb98@users.noreply.github.com>
2023-01-13 20:03:35 +0530

    Add files via upload

---
+++ backslash.py
@@ -0,0 +1,20 @@
+"""
+Day: Friday
+Date: 13th Jan 2023
+Topic: backslash
+Time: 5PM
+Author: Areeb Ahmed
+"""
+# print('''Hello Everyone welcome to python,
+# class which is running in 2nd floor at 
+# josh innovations''')
+
+#print('''Hello Everyone welcome to python, \
+#class which is running in 2nd floor at \
+#josh innovations''')
+
+# print("we are learning python programming from, \
+ # josh innovations in 2nd floor at 10:30AM")
+
+# print('Tfshdgjsgdbjavfahhfggancvcnsiergd\
+# uiegfcchgdjjbgfkwiuyyywkjkfkvgvsfhfdygffgudjfhkf')
